c8f1f030f9f4f1b51a5b720169850b404a3e3828
Jay <jay@Jays-Air.wifi.gsu.edu>
2023-01-25 14:49:39 -0500

    Added basic function for fetching tweets & set up environmental variables

---
+++ api.py
@@ -0,0 +1,26 @@
+from dotenv import load_dotenv
+import os
+import requests
+
+# Load environmental variables. By default, dotenv searches current directory for .env file
+# A custom filepath to the .env file can be supplied as the argument
+load_dotenv()
+
+# Fetch bearer token required for https authentication
+BEARER_TOKEN = os.getenv("BEARER_TOKEN")
+
+# https header
+headers = {"Authorization": "Bearer {}".format(BEARER_TOKEN)}
+
+# this function converts a Twitter handle into user ID.
+# method = GET, url = query string, headers = security token declared above.
+def getID(handle):
+    url = "https://api.twitter.com/2/users/by/username/{}".format(handle)
+    response = requests.request("GET", url, headers=headers)
+    return response
+
+# grab Tweets posted by an user ID:
+def getTweets(id):
+    url = "https://api.twitter.com/2/users/{}/tweets".format(id)
+    response = requests.request("GET", url, headers=headers)
+    return response
