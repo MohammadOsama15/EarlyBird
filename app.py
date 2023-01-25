5c3c896443db9839e2dceeddd7866f578cbc1cf3
MahreenAsghar <a.mahreen84@gmail.com>
2023-01-25 10:09:08 -0500

    Created HTML pages and linked them to their routes in app.py

--- app.py
+++ app.py
@@ -1,11 +1,43 @@
 import flask
+from flask import send_file
 
 
 app = flask.Flask(__name__)
-
 @app.route('/')
+def welcome():
+    return send_file('./templates/welcome.html')
+
+@app.route('/login')
+def login():
+    return send_file("./templates/login.html")
+
+@app.route('/signup')
+def signup():
+    return send_file("./templates/signup.html")
+
+@app.route('/logout')
+def logout():
+    return send_file("./templates/logout.html")
+    
+@app.route('/search')
 def search():
-    return 'Search Page'
+    return send_file("./templates/search.html")
+
+@app.route('/information')
+def information():
+    return send_file("./templates/info.html")
+
+@app.route('/userTopic')
+def userTopic():
+    return send_file("./templates/userTopic.html")
+
+@app.route('/userSentiment')
+def userSentiment():
+    return send_file("./templates/userSentiment.html")
+
+@app.route('/topicSentiment')
+def topicSentiment():
+    return send_file("./templates/topicSentiment.html")
 
 if __name__ =='__main__':
     app.run()
