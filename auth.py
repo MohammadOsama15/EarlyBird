677fb66ac01d5096ea081f07d93bec8a920fa697
jayliu223 <99854273+jayliu223@users.noreply.github.com>
2023-01-31 13:11:46 -0500

    followed tutorial @ digital ocean and added flask login with pw hashing

--- auth.py
+++ auth.py
@@ -1,16 +1,71 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask_login import login_user, login_required, logout_user
+from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
 from . import db
 
 auth = Blueprint('auth', __name__)
+
 
 @auth.route('/login')
 def login():
     return render_template('login.html')
 
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    # check whether email exists and hash matches
+    if not user or not check_password_hash(user.password, password):
+        flash("Please check your login details and try again.")
+        return redirect(url_for("auth.login"))
+
+    login_user(user, remember=remember)
+    return redirect(url_for('main.profile'))
+
+
 @auth.route('/signup')
 def signup():
     return render_template('signup.html')
 
+
+# function for submitting user registration info via http POST
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+
+    email = request.form.get('email')
+    firstname = request.form.get('firstname')
+    lastname = request.form.get('lastname')
+    password = request.form.get('password')
+
+    # checks whether email is already in use
+    user = User.query.filter_by(email=email).first()
+    print(user)
+
+    # if record exists, force signup again
+    if user:
+        return redirect(url_for('auth.signup'))
+
+    # if execution reaches this line (e.g. doesn't get short-circuited), create new user and hash password entry.
+    new_user = User(email=email, firstname=firstname, lastname=lastname,
+                    password=generate_password_hash(password, method='sha256'))
+
+    # add the user
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect(url_for('auth.login'))
+
+
 @auth.route('/logout')
+@login_required
 def logout():
-    return 'Logout'
+    logout_user()
+    print(url_for('main.index'))
+    return redirect(url_for('main.index'))
--- main.py
+++ main.py
@@ -1,31 +1,44 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from flask_login import login_required, current_user
 from . import db
 
 main = Blueprint('main', __name__)
 
+
 @main.route('/')
-def welcome():
+def index():
     return render_template("index.html")
 
+
+# log in required to view private details
 @main.route('/profile')
+@login_required
 def profile():
-    return render_template("profile.html")
-    
+
+    return render_template("profile.html", name=current_user.firstname)
+
+
+# log in required to use search feature?
 @main.route('/search')
+@login_required
 def search():
     return render_template("search.html")
+
 
 @main.route('/information')
 def information():
     return render_template("information.html")
 
+
 @main.route('/userTopic')
 def userTopic():
     return render_template("userTopic.html")
 
+
 @main.route('/userSentiment')
 def userSentiment():
     return render_template("userSentiment.html")
+
 
 @main.route('/topicSentiment')
 def topicSentiment():
