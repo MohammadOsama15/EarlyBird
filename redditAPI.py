8c7f8989b392ae8daa38c76df4338cbc3e060ea8
MohammadOsama15 <osama_Farhat@Outlook.com>
2023-02-16 22:34:05 -0500

    adding the cap functionality to specify how many you need to pull from the api

--- redditAPI.py
+++ redditAPI.py
@@ -21,19 +21,32 @@ def get_access_token():
     headers = {'User-Agent': 'MyAPI/0.0.1'}
     res = requests.post('https://www.reddit.com/api/v1/access_token',
                         auth=auth, data=data, headers=headers)
-    d = res.json()
-    if 'access_token' not in d:
-        print(f"Error: {d['message']}")
-        exit()
+    try:
+        d = res.json()
+        if 'access_token' not in d:
+            print(f"Error: {d['message']}")
+            return None
 
-    return 'bearer ' + d['access_token']
+        return f"bearer {d['access_token']}"
+    except json.decoder.JSONDecodeError:
+        print(f"Error: Could not decode JSON response from Reddit API")
+        return None
+    except requests.exceptions.RequestException as e:
+        print(f"Error: {e}")
+        return None
 
-def get_posts(query):
+def get_posts(query, cap=None):
     if query is None:
         print("Error: Query parameter is None")
         return None
 
+    if cap is None:
+        cap = 50
+
     token = get_access_token()
+    if token is None:
+        return None
+
     headers = {'Authorization': token, 'User-Agent': 'MyAPI/0.0.1'}
     base_url = 'https://oauth.reddit.com'
     response = requests.get(base_url + '/api/v1/me', headers=headers)
@@ -52,19 +65,22 @@ def get_posts(query):
         print("Reddit user not found.")
         return None
 
-    posts = res.json()['data']['children']
+    posts = res.json()['data']['children'][:cap]
     titles = set()
-    for post in posts[:min(50, len(posts))]:
+    for post in posts:
         data = post['data']
         title = data.get('title', data.get('link_title', None))
         if title is not None and title not in titles:
-            titles.add(title)
+            titles.add(title) #Adds clean title to the set.
             print(title)
     if not titles:
-        return ("Reddit user has no posts.")
+        return "Reddit user has no posts."
     return titles
 
+def search_posts(query, cap=None):
+    titles = get_posts(query, cap)
+    if titles is None:
+        return None
 
-def search_posts(query):
-    titles = get_posts(query)
+    # Perform additional processing on the returned titles, if needed.
     return titles
