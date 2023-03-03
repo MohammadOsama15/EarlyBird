cf732f604e66c9852e3f2ec02c6d68cb715d3c7f
MahreenAsghar <a.mahreen84@gmail.com>
2023-03-03 13:43:54 -0500

    fixed merge conflicts

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request
 from flask_login import login_required, current_user
+from flask import Blueprint, render_template, redirect, url_for, request, flash
 from . import db
 from . import cache
 from redditAPI import get_posts
@@ -11,6 +12,8 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def index():
+    if current_user.is_authenticated:
+        return redirect(url_for('main.search'))
     return render_template("index.html")
 
 
